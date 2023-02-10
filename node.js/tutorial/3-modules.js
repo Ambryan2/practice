8bca2f7b9f784237bfe07db1552a250fa1951dab
Your Name <alobryan2@gmail.com>
2023-02-10 09:16:59 -0500

    initial commit

---
+++ node.js/tutorial/3-modules.js
@@ -0,0 +1,12 @@
+// CommonJS, every file in node is a module (by default)
+//Modules - Encapsulated Code (only share minimum)
+const names = require('./4-name') //have to start with dot
+const sayHi = require('./5-utils')
+
+const data = require('./6- alternative-flavor')
+console.log(data)
+
+require('./7-mind-grenade') //when you import a module you also invoke it
+
+sayHi(names.john)
+sayHi(names.peter)
---
+++ node.js/tutorial/6- alternative-flavor.js
@@ -0,0 +1,8 @@
+// does the same as 3,4,5-modules
+
+module.exports.items = ['item1','item2'];
+const person = {
+    name: 'bob',
+}
+
+module.exports.singlePerson = person 
