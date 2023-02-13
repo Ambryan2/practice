d1f2d2bee7dc536d0f51d5ee0823c1373cf17967
Your Name <alobryan2@gmail.com>
2023-02-13 16:22:37 -0500

    reverse string

---
+++ code-problems/javascript/6-reverse-string.js
@@ -0,0 +1,14 @@
+// reverse a string without using any built in functions
+
+let example="hello"
+let example2 = 3
+
+function reverse(word){
+    let answer = ""
+    for (let i=word.length-1; i>=0; i--){
+        answer = answer + word[i]
+    }
+    return answer
+}
+
+console.log(reverse(example))
