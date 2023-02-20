e7541534da1f91426f8a617c85e7754dd4b5d966
Your Name <alobryan2@gmail.com>
2023-02-20 09:30:03 -0500

    async, http, npm

--- node.js/tutorial/10-fs-sync.js
+++ node.js/tutorial/10-fs-sync.js
@@ -1,8 +1,11 @@
 //this same as fs.readFilesync
 const {readFileSync, writeFileSync} = require('fs')
 
+console.log('start') //demonstrate difference between sync and async
+
 const first = readFileSync('./content/first.txt','utf8')
 const second = readFileSync('./content/second.txt','utf8')
+
 
 // write file creates new file with content
 writeFileSync(
@@ -10,4 +13,8 @@ writeFileSync(
     `Here is the result: ${first}, ${second}`,
     {flag: 'a'} //this lets it make a new line each time
 )
+
+console.log('done with task')
+console.log('starting next task')
+
 console.log(first, second)
